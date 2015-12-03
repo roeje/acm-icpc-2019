8d4061bfa84552e3ddec3a933b5591923d4b5cb8
Jesse Roe <jessedroe@gmail.com>
2015-12-03 16:18:50 +0800

    Final Version!

--- icpc2019.cpp
+++ icpc2019.cpp
@@ -1,4 +1,6 @@
-
+//
+// Created by Jesse on 12/2/2015.
+//
 
 #include <iostream>
 #include <algorithm>
@@ -6,10 +8,15 @@
 
 using namespace std;
 
+/*Main logic method*/
 string readData(istream& input) {
     string c = "";
     int itr = 1;
+
+    /*Loop over each test case*/
     while (input >> c && c != "STOP") {
+
+        /*Unique case for spacing between individual tests*/
         if(itr != 1){
             cout << endl;
         }
@@ -17,51 +24,43 @@ string readData(istream& input) {
         cout << "Dataset #" << itr <<":  The initial order is " << originalOrder << "." << endl;
         int tests = 0;
         input >> tests;
+
+        /*loop through each character removal*/
         while (tests > 0) {
             string charToRemove = "";
             input >> charToRemove;
             int location = originalOrder.find(charToRemove);
-//            cout << location << endl;
-
-            if(location <= originalOrder.size()/2) {
-                string tempCars = originalOrder.substr(0, location);
-//                cout << tempCars << endl;
+            string before = originalOrder.substr(0, location);
+            string after = originalOrder.substr(location + 1, originalOrder.length());
+            if(before.length() <= after.length()) {
                 originalOrder.erase(0,location + 1);
-//                cout << originalOrder << endl;
-                for (auto itr = tempCars.begin(); itr != tempCars.end(); ++itr) {
+                for (auto itr = before.begin(); itr != before.end(); ++itr) {
                     string s = "";
-                    s +=(*itr);
+                    s += (*itr);
                     originalOrder = s += originalOrder;
-//                    cout << originalOrder << endl;
                 }
             }
             else {
-                string tempCars = originalOrder.substr(location + 1, originalOrder.size());
-//                cout << tempCars << endl;
                 originalOrder.erase(location, originalOrder.size());
-//                cout << originalOrder << endl;
-                for (auto itr = tempCars.rbegin(); itr != tempCars.rend(); ++itr) {
+                for (auto itr = after.rbegin(); itr != after.rend(); ++itr) {
                     string s = "";
                     s +=(*itr);
                     originalOrder += s;
-//                    cout << originalOrder << endl;
                 }
             }
-            if(tests != 1) {
-                cout << "After " << charToRemove << " leaves, the order is " << originalOrder << "." << endl;
-            }
-            else {
-                cout << "After " << charToRemove << " leaves, the order is " << originalOrder << ".";
-            }
+
+            /*Output after each removal*/
+            cout << "After " << charToRemove << " leaves, the order is " << originalOrder << "." << endl;
             tests--;
         }
         itr++;
-//        cout << endl;
     }
 }
 
 int main(){
 #ifndef ONLINE_JUDGE
+
+    /*Test data with edge cases*/
     string testData = "ABCDEFG\n"
             "3\n"
             "E\n"
@@ -70,36 +69,46 @@ int main(){
             "HIWORLD\n"
             "1\n"
             "H\n"
+            "ABC\n"
+            "3\n"
+            "A\n"
+            "B\n"
+            "C\n"
+            "HIWORLD\n"
+            "1\n"
+            "O\n"
+            "H\n"
+            "1\n"
+            "H\n"
+            "A\n"
+            "0\n"
+            "FDJK\n"
+            "2\n"
+            "D\n"
+            "F\n"
+            "FDJK\n"
+            "2\n"
+            "D\n"
+            "F\n"
+            "FDJK\n"
+            "2\n"
+            "J\n"
+            "K\n"
+            "FDJK\n"
+            "2\n"
+            "K\n"
+            "J\n"
+            "HIWORLD\n"
+            "1\n"
+            "D\n"
+            "HIWORLD\n"
+            "1\n"
+            "O\n"
             "STOP";
+
     istringstream data(testData);
 #else
     istream& data = cin;
 #endif
     readData(data);
-    return 0;
 }
-
-//ABC
-//3
-//A
-//B
-//C
-//HIWORLD
-//1
-//O
-//H
-//1
-//H
-//A
-//0
-//FDJK
-//2
-//D
-//F
-//HIWORLD
-//1
-//D
-//HIWORLD
-//1
-//O
-//STOP
